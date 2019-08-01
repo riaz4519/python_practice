506e627440bd48501d2360b1148520a8f7a4f8e0
unknown <riaz.i3216@gmail.com>
2019-08-01 13:25:28 +0600

    at new office marge

---
+++ programiz/_15_input_output.py
@@ -0,0 +1,17 @@
+#noraml print
+
+print('This is a normal print')
+
+#print with a variable
+a = 5
+print("The value of a is",a)
+
+#print with sep and end
+
+print(1,2,3,4,sep='#',end='&')
+
+#output formating str.format()
+
+first_name ,last_name = 'Fahim','Riaz'
+
+print('My first Name is {} last Name is{}'.format(first_name,last_name))
