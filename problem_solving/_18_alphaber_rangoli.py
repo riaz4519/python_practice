3a46b4cc8bcf68f6e4d80592f1f7352d3e93e036
unknown <riaz.i3216@gmail.com>
2019-08-04 21:41:45 +0600

    after mat

--- problem_solving/_18_alphaber_rangoli.py
+++ problem_solving/_18_alphaber_rangoli.py
@@ -1,5 +1,13 @@
+
 n = int(input())
+l1 = []
+pattern = []
+for i in range(n-1,-1,-1):
+    pattern.append(("".join(l1[0:n-i])+chr(97+i)+"-"+"".join(l1[::-1])).center(4*n-3,"-"))
+    l1.append(chr(97+i)+"-")
+else:
+    pattern[n-1] = pattern[n-1][0:len(pattern[n-1])-1]
 
-pattern = [ chr(97+i).center(n*3,'-') for i in range(n-1,0,-1)]
+print("\n".join(pattern+pattern[0:len(pattern)-1][::-1]))
 
-print("\n".join(pattern))
+
