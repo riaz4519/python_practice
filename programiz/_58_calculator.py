8487d125386a386e7e8324aec66d4652e07f857d
riaz4519 <riaz.i3216@gmail.com>
2019-12-17 00:31:22 +0600

    data

--- programiz/_58_calculator.py
+++ programiz/_58_calculator.py
@@ -1,8 +1,30 @@
-
-
-def sum():
-    pass
+def add(num1,num2):
+    return  num1 + num2
+def subtract(num1,num2):
+    return  num1 - num2
+def multiply(num1,num2):
+    return  num1 * num2
+def divide(num1,num2):
+    return  num1 / num2
 
 
 if __name__ == "__main__":
-    sum()
+    print("Please select operation -\n" \
+          "1. Add\n" \
+          "2. Subtract\n" \
+          "3. Multiply\n" \
+          "4. Divide\n")
+    select = input("Select operations form 1, 2, 3, 4 :")
+
+    number_1 = int(input("Enter first number"))
+    number_2 = int(input("Enter second number"))
+
+    if select == "1":
+
+        print(number_1,"+",number_2,"=",add(number_1,number_2))
+    elif select == "2":
+        print(number_1,"-",number_2,"=",subtract(number_1,number_2))
+    elif select == "3":
+        print(number_1,"*",number_2,"=",multiply(number_1,number_2))
+    elif select == "4":
+        print(number_1,"/",number_2,"=",divide(number_1,number_2))
---
+++ programiz/_64_input.py
@@ -0,0 +1,13 @@
+# input1 = input()
+# print(input1)
+
+#type casting int
+
+# num1 = int(input())
+# num2 = int(input())
+#
+# print(num1 + num2)
+
+#type cast float
+
+#type cast str
