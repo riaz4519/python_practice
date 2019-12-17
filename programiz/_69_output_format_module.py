1318625754b86d0bfbcb76461fb2e14dbd011711
unknown <riaz.i3216@gmail.com>
2019-12-17 21:18:00 +0600

    after addin 70

---
+++ programiz/_69_output_format_module.py
@@ -0,0 +1,17 @@
+#module format
+
+print("Geeks : % 2d,Portal : %5.2f" %(155,05.33))
+
+#with the format method
+
+print("i love {} for {}".format("Geeks","Geeks"))
+
+#with position format
+print("i love {1} for {2}".format("for","Geeks","Geeks"))
+
+#format field with module value changes
+print("{0:2d}".format(17))
+
+#namde position
+
+print("{data}".format(data="data"))
