1b1efc3dc5c4d4228a80ca935a6f3fabc14da5e2
riaz4519 <riaz.i3216@gmail.com>
2019-12-19 02:00:24 +0600

    after 71

---
+++ programiz/_71_data_types.py
@@ -0,0 +1,37 @@
+#list
+l = [1,"a","string",1+2]
+
+print(l)
+
+#append
+l.append(6)
+print(l)
+#pop
+l.pop()
+print(l)
+
+#tupple
+
+tup = (1,"a","string",1+2)
+
+print(tup)
+print(tup[1])
+
+#iteration python
+
+i = 1
+while (i <10):
+    i += 1
+    print(i,end=" ")
+
+#iteration in string
+s = "Hello World"
+for i in s:
+    print(i)
+#iteraton loop on list
+l = [1,5,6,8,6,9,4,6,7]
+for i in l:
+    print(i,end=" ",)
+
+for i in range(0,10):
+    print(i)
